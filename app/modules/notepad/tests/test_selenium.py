7190a882df3ca03c6eef4c73a1f79af4798f1b5d
Nuno del Pino <nundelesc@alum.us.es>
2025-10-28 08:41:09 +0100

    practicas

--- app/modules/notepad/tests/test_selenium.py
+++ app/modules/notepad/tests/test_selenium.py
@@ -3,6 +3,7 @@ import time
 
 from core.environment.host import get_host_for_selenium_testing
 from core.selenium.common import initialize_driver, close_driver
+from selenium.webdriver.common.by import By
 
 
 def test_notepad_index():
@@ -31,5 +32,29 @@ def test_notepad_index():
         close_driver(driver)
 
 
+def test_notepadcreate():
+    driver = initialize_driver()
+    host = get_host_for_selenium_testing()
+    
+    driver.get(f'{host}/login?next=%2Fnotepad')
+    driver.set_window_size(706, 923)
+    driver.find_element(By.ID, "email").click()
+    driver.find_element(By.ID, "email").send_keys("user1@example.com")
+    driver.find_element(By.ID, "password").click()
+    driver.find_element(By.ID, "password").send_keys("1234")
+    driver.find_element(By.ID, "submit").click()
+    driver.get(f'{host}/notepad/create')
+    driver.find_element(By.CSS_SELECTOR, "form > div:nth-child(3)").click()
+    driver.find_element(By.ID, "title").click()
+    driver.find_element(By.ID, "title").send_keys("Prueba")
+    driver.find_element(By.ID, "body").click()
+    driver.find_element(By.ID, "body").send_keys("Prueba")
+    driver.find_element(By.CSS_SELECTOR, ".content").click()
+    driver.find_element(By.ID, "submit").click()
+    
+    close_driver(driver)
+
+
 # Call the test function
 test_notepad_index()
+test_notepadcreate()
